41ee066d324e038243c7d47d55b54fe6ef1d4ea9
Andreas Ludwig <andreasludwig369@gmail.com>
2021-12-02 14:59:10 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# minimal-csp-error
+
+Prerequisites: node
+
+How to start:
+1. npm install
+2. node bin/www
--- app.js
+++ app.js
@@ -2,18 +2,19 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+
+app.use((req, res, next) => {
+    res.set("Content-Security-Policy", "default-src 'self'; img-src 'self' 'strict-dynamic'");
+    next()
+})
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 var indexRouter = express.Router();
 
 indexRouter.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
-
-
-app.use((req, res, next) => {
-    res.set("Content-Security-Policy", "default-src 'self'; img-src 'self' 'strict-dynamic' http://not-localhost.com");
-    next()
-})
-app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 module.exports = app;
